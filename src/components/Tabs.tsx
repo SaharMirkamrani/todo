08363aa0816b0c557608f2800cad1d6931d601f2
SaharMirkamrani <saharmirkamrani@gmail.com>
2025-01-03 17:55:10 +0330

    chore: add font, material ui, initial tabs structure

---
+++ src/components/Tabs.tsx
@@ -0,0 +1,72 @@
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import * as React from 'react';
+
+export default function LabTabs() {
+  const [value, setValue] = React.useState('1');
+
+  const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box className="w-full">
+      <TabContext value={value}>
+        <Box className="bg-white rounded-t-2xl">
+          <TabList
+            onChange={handleChange}
+            aria-label="lab API tabs example"
+            className="flex justify-evenly"
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            indicatorColor="black"
+          >
+            <Tab
+              label="Today's Task"
+              value="1"
+              sx={{
+                fontFamily: 'var(--font-poppins)',
+                fontSize: '12px',
+                fontWeight: 'bold',
+                textTransform: 'none',
+                padding: '24px 30px 15px',
+                width: '50%',
+                color: value === '1' ? 'black' : 'inherit',
+                borderBottom: value === '1' ? '1px solid black' : 'none',
+                '&.Mui-selected': {
+                  color: 'black',
+                },
+              }}
+            />
+            <Tab
+              label="Tomorrow's Task"
+              value="2"
+              sx={{
+                fontFamily: 'var(--font-poppins)',
+                fontSize: '12px',
+                width: '50%',
+                fontWeight: 'bold',
+                textTransform: 'none',
+                padding: '24px 30px 15px',
+                color: value === '2' ? 'black' : 'inherit',
+                borderBottom: value === '2' ? '1px solid black' : 'none',
+                '&.Mui-selected': {
+                  color: 'black',
+                },
+              }}
+            />
+          </TabList>
+        </Box>
+
+        <div className="bg-white bg-opacity-80 rounded-b-2xl">
+          <TabPanel value="1">Today`&apos;`s Task</TabPanel>
+          <TabPanel value="2">Tomorrow`&apos;`s Task</TabPanel>
+        </div>
+
+      </TabContext>
+    </Box>
+  );
+}
